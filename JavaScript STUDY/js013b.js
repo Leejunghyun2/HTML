da2c103a682adaef07f43e1848ad17eb4419f585
jung <dlwjdgusehd@naver.com>
2022-06-09 22:55:36 +0900

    13 13 13 13 13 13 13 13 13 13 13 13 13

--- JavaScript STUDY/js013b.js
+++ JavaScript STUDY/js013b.js
@@ -1,3 +1,6 @@
+
+
+
 function Student(name,pw,web1,web2){
     this.name=name;
     this.pw=pw;
@@ -11,13 +14,5 @@ stu[0] = new Student('홍정아',12345,88,77);
 stu[1] = new Student('조민규',54321,82,86);
 stu[2] = new Student('이은준',1357,64,78);
 stu[3] = new Student('신은수',24680,94,86);
-
-function score(){
-for(i=0;i<stu.length;i++){
-    document.getElementById('name'+i).innerHTML = stu[i].name;
-    document.getElementById("middle"+i).innerHTML = stu[i].middle;
-    document.getElementById("final"+i).innerHTML = stu[i].final;
-    document.getElementById("total"+i).innerHTML = stu[i].total;
-    }
-}
+stu[4] = new Student('송나현',112233,65,75);
 
