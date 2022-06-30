ee85a133576679a714bb2a5c0bc719d8f4272f05
LeeJungHyun <dlwjdgusehd@naver.com>
2022-06-30 17:25:08 +0900

    hkitsite

---
+++ CSS STUDY/responsiveExam이정현.js
@@ -0,0 +1,6 @@
+let menu = document.querySelectorAll('.navMenu');
+
+console.log("🚀 ~ file: responsiveExam이정현.js ~ line 3 ~ menu.length", menu.length)
+for(i=0;i<menu.length;i++){
+    menu[i].classList('d');
+}
